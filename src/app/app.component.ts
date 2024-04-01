ad068a7d6b841a22b9f6778c105d2bdc99f4e126
bgrinste <bgrinste@netapp.com>
2024-04-01 22:14:05 +0300

    without viewProivders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {DanceStyleFormComponent} from "./dance-style-form/dance-style-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, ReactiveFormsModule, DanceStyleFormComponent],
+  template: `
+    <form [formGroup]="form" (ngSubmit)="submit()">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" formControlName="name">
+      </div>
+      <div>
+        <label for="name">Email</label>
+        <input type="text" id="email" formControlName="email">
+      </div>
+      <app-dance-style-form [form]="form"></app-dance-style-form>
+      <button>Submit</button>
+    </form>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dynamic-forms-dance';
+
+  form = new FormGroup({
+    name: new FormControl<string>(''),
+    email: new FormControl<string>(''),
+    styles: new FormGroup({
+      style: new FormControl<string>(''),
+      years: new FormControl<number>(0)
+    })
+  });
+
+
+  submit() {
+    console.log(this.form.value);
+    this.form.reset();
+  }
 }
